292a88e96e1eb069129a0837384600841b976003
Muhammad Adisurya Pratama <91768688+madisuryapr@users.noreply.github.com>
2023-04-05 16:34:37 +0700

    Update Import and Insert Data.sql

--- Cashless Payments Around Indonesia/Preparation/Import and Insert Data.sql
+++ Cashless Payments Around Indonesia/Preparation/Import and Insert Data.sql
@@ -1,7 +1,7 @@
 /* This Query Examines the process of Importing Data from .csv Files
    and Inserting Multiple Values into Tables */
 
--- 1. Import Data for Non-cash Payment Systems Transactions Value
+-- Task No.1: Import Data for Cashless Payments System Transactions Value
 COPY
     transactions_value (time_date, province_isocode,
                        region_isocode, payment_system_name,
@@ -10,8 +10,10 @@ FROM
     'D:\Data Analytics\Project\Cashless Payments Around Indonesia\Dataset\transactions_value.csv'
 DELIMITER ';'
 CSV HEADER;
+-- This query is PostgreSQL-specific, therefore this query is only available to PostgreSQL
+-- The data path within this query can be altered based on your own data directory
 
--- 2. Import Data for Non-cash Payment Systems Transactions Volume
+-- Task No.2: Import Data for Non-cash Payment Systems Transactions Volume
 COPY
     transactions_volume (time_date, province_isocode,
                        region_isocode, payment_system_name,
@@ -22,7 +24,7 @@ DELIMITER ';'
 CSV HEADER;
 -- Each Tables consist of 4896 rows of data
 
--- 3. Import Data for province_id Table
+-- Task No.3: Import Data for province_id Table
 -- This process will insert 34 Data of Indonesia's Provinces
 COPY
     province_id (province_code, province_name)
@@ -31,7 +33,7 @@ FROM
 DELIMITER ';'
 CSV HEADER;
 
--- 4. Insert Data Into noncashps_id Table
+-- Task No.4: Insert Data Into noncashps_id Table
 INSERT INTO
     noncashps_id (payment_system_id, payment_system_name)
 VALUES ('DBTC', 'Debit Cards'),
@@ -42,7 +44,7 @@ VALUES ('DBTC', 'Debit Cards'),
 -- This query will insert 5 Non-cash Payment Systems which are
 -- recognized in Indonesia
 
--- 5. Insert Data Into region_id Table
+-- Task No.5: Insert Data Into region_id Table
 INSERT INTO
     region_id (region_isocode, region_name)
 VALUES ('NU', 'Nusa Tenggara'),

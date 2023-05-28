ca1e6061a5cf18f794d9fdec9239876d78dc58e6
Muhammad Adisurya Pratama <91768688+madisuryapr@users.noreply.github.com>
2023-05-28 21:13:17 +0700

    Add files via upload

--- Stocks Performance of Indonesian Companies/Window Functions/Window Functions I.sql
+++ Stocks Performance of Indonesian Companies/Window Functions/Window Functions I.sql
@@ -1,25 +1,25 @@
 /* Window Functions I */
 /* In this file, I demonstrate and introduce the utilization of Window Functions
-   withini SQL Server environment
+   within SQL Server environment
 */
 
 -- Use stocks_performance_Indonesia database
 USE stocks_performance_Indonesia;
 
--- 1. Retrieve row number for stock_performance table
+-- No.1: Retrieve row number for stock_performance table
 SELECT
     ROW_NUMBER() OVER(
         ORDER BY stock_ticker
         ) AS row_number,
     stock_ticker AS ticker,
     date AS period,
-    open_price,
-    high_price,
-    low_price,
-    close_price
+    price_open,
+    price_high,
+    price_low,
+    price_close
 FROM stock_performance;
 
--- 2. Obtain row number for stock_performance table for each stock ticker
+-- No.2: Obtain row number for stock_performance table for each stock ticker
 SELECT 
     ROW_NUMBER() OVER(
         PARTITION BY stock_ticker
@@ -27,41 +27,41 @@ SELECT
     ) AS row_number_partitioned,
     stock_ticker AS ticker,
     date AS period,
-    open_price,
-    low_price,
-    high_price,
-    close_price
+    price_open,
+    price_low,
+    price_high,
+    price_close
 FROM stock_performance;
 
--- 3. Extract previous 30 days stocks typical price for each stock ticker
+-- No.3: Extract previous 30 days stocks typical price for each stock ticker
 SELECT
     stock_ticker AS ticker,
     date AS period,
-    ((high_price + low_price + close_price)/3) AS typical_price,
-    LAG((high_price + low_price + close_price)/3, 30) 
+    ((price_high + price_low + price_close)/3) AS typical_price,
+    LAG((price_high + price_low + price_close)/3, 30)
     OVER ( 
         PARTITION BY stock_ticker
         ORDER BY stock_ticker
     ) AS typical_price_lag30
 FROM stock_performance;
 
--- 4. Calculate the next 60 days of stocks high, low, and close prices
+-- No.4: Calculate the next 60 days of stocks high, low, and close prices
 SELECT
     sp.stock_ticker AS ticker,
     sl.company_name AS company_name,
     sp.date AS period,
-    high_price,
-    low_price,
-    close_price,
-    LEAD(high_price, 60) OVER(
+    price_high,
+    price_low,
+    price_close,
+    LEAD(price_high, 60) OVER(
         PARTITION BY stock_ticker
             ORDER BY stock_ticker
     ) AS high_price_lead60,
-    LEAD(low_price, 60) OVER(
+    LEAD(price_low, 60) OVER(
         PARTITION BY stock_ticker
             ORDER BY stock_ticker
     ) AS high_price_lead60,
-    LEAD(close_price, 60) OVER(
+    LEAD(price_close, 60) OVER(
         PARTITION BY stock_ticker
             ORDER BY stock_ticker
     ) AS close_price_lead60
@@ -69,13 +69,13 @@ FROM stock_performance AS sp
 INNER JOIN stock_listing AS sl 
     ON sp.stock_ticker = sl.stock_code;
 
--- 5. Determine the culumative distribution of stock's monthly average typical price for each company
+-- No.5: Determine the cumulative distribution of stock's monthly average typical price for each company
 SELECT
     sp.stock_ticker AS ticker,
     sl.company_name AS company,
     DATEPART(year, sp.date) AS year,
     DATEPART(month, sp.date) AS month,
-    AVG((high_price + low_price + close_price)/3) AS monthly_typical_price,
+    AVG((price_high + price_low + price_close)/3) AS monthly_typical_price,
     CAST(CUME_DIST() OVER(
         PARTITION BY DATEPART(year, sp.date)
         ORDER BY stock_ticker
